a0f0146662666ed62c81861d65b67782b1618eaa
Abishek Bardewa <way2abishek@gmail.com>
2024-05-25 21:48:27 +0530

    Updates on dropdown and category slider

--- src/components/common/Categories.jsx
+++ src/components/common/Categories.jsx
@@ -1,14 +1,60 @@
 import React from 'react';
 import { CATEGORIES } from '../../constants/categories';
+import Slider from 'react-slick';
+import { FaCircleChevronLeft, FaCircleChevronRight } from 'react-icons/fa6';
+function NextArrow(props) {
+	const { className, style, onClick } = props;
+	return <FaCircleChevronRight className={className} style={{ ...style, display: 'block', color: '#000' }} onClick={onClick} />;
+}
+
+function PrevArrow(props) {
+	const { className, style, onClick } = props;
+	return <FaCircleChevronLeft className={className} style={{ ...style, display: 'block', color: '#000' }} onClick={onClick} />;
+}
 const Categories = () => {
+	const settings = {
+		dots: false,
+		navs: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 9,
+		slidesToScroll: 3,
+		nextArrow: <NextArrow />,
+		prevArrow: <PrevArrow />,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 6,
+					slidesToScroll: 3,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 5,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 2,
+				},
+			},
+		],
+	};
 	return (
-		<div className="my-12 mx-auto max-w-7xl px-4 grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10 gap-3 lg:gap-8 md:gap-5">
-			{CATEGORIES.map((ele) => (
-				<div key={ele.tagName} className="group flex justify-center items-center flex-col transition-transform duration-200 cursor-pointer gap-2">
-					<img src={ele.icon} alt={ele.title} className="object-cover w-8 h-8 opacity-80 group-hover:opacity-100" />
-					<div className="text-xs whitespace-nowrap font-normal group-hover:font-semibold">{ele.title}</div>
-				</div>
-			))}
+		<div className="px-8 my-12  max-w-7xl mx-4  md:mx-16 xl:mx-auto">
+			<Slider {...settings}>
+				{CATEGORIES.map((ele) => (
+					<div key={ele.tagName} className="group !flex justify-center items-center !flex-col transition-transform duration-200 cursor-pointer gap-2">
+						<img src={ele.icon} alt={ele.title} className="object-cover w-8 h-8 opacity-80 group-hover:opacity-100" />
+						<div className="text-xs whitespace-nowrap font-normal group-hover:font-semibold">{ele.title}</div>
+					</div>
+				))}
+			</Slider>
 		</div>
 	);
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
+
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
